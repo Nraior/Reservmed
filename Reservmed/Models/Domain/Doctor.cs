cfc2d32800061a80f71930be135f0daea81091e2
Damian Nowakowski <dnowakowski200@gmail.com>
2026-04-25 00:28:16 +0200

    feat: Specializations DB updates & Doctor updates

--- Reservmed/Models/Domain/Doctor.cs
+++ Reservmed/Models/Domain/Doctor.cs
@@ -8,24 +8,21 @@ namespace Reservmed.Models.Domain
 
         public int Id { get; set; }
 
-        [Required]
-        public string ApplicationUserId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public required string ApplicationUserId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
 
-        [Required]
         public DateTime BirthDate { get; set; }
 
         [StringLength(20)]
-        public string LicenseNumber { get; set; }
+        public required string LicenseNumber { get; set; }
 
-        public List<String> Specializations { get; set; } = new List<string>();
+        public ICollection<Specialization> Specializations { get; set; } = new List<Specialization>();
 
         public string? ProfilePictureUrl { get; set; }
-
         public string? Description { get; set; }
         // navigation property
-        public ApplicationUser User { get; set; }
+        public ApplicationUser? User { get; set; }
         public bool IsVerifiedByAdmin { get; set; } = false;
     }
 }
