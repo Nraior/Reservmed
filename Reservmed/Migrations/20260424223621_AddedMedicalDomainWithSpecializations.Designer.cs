0e6b43c84bd6d81d0937fee90466921fd5c426ac
Damian Nowakowski <dnowakowski200@gmail.com>
2026-04-25 00:38:32 +0200

    chore: migration files

--- Reservmed/Migrations/20260424223621_AddedMedicalDomainWithSpecializations.Designer.cs
+++ Reservmed/Migrations/20260424223621_AddedMedicalDomainWithSpecializations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reservmed.Data;
 
@@ -11,9 +12,11 @@ using Reservmed.Data;
 namespace Reservmed.Migrations
 {
     [DbContext(typeof(ReservmedDBContext))]
-    partial class ReservmedDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260424223621_AddedMedicalDomainWithSpecializations")]
+    partial class AddedMedicalDomainWithSpecializations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Reservmed/Migrations/20260424223621_AddedMedicalDomainWithSpecializations.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Reservmed.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMedicalDomainWithSpecializations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Specializations",
+                table: "Doctors");
+
+            migrationBuilder.CreateTable(
+                name: "Specializations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specializations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DoctorSpecializations",
+                columns: table => new
+                {
+                    SpecializationId = table.Column<int>(type: "int", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    DateAcquired = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorSpecializations", x => new { x.SpecializationId, x.DoctorId });
+                    table.ForeignKey(
+                        name: "FK_DoctorSpecializations_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DoctorSpecializations_Specializations_SpecializationId",
+                        column: x => x.SpecializationId,
+                        principalTable: "Specializations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Specializations",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Cardiology" },
+                    { 2, "Dermatologist" },
+                    { 3, "Internist" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorSpecializations_DoctorId",
+                table: "DoctorSpecializations",
+                column: "DoctorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DoctorSpecializations");
+
+            migrationBuilder.DropTable(
+                name: "Specializations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Specializations",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
