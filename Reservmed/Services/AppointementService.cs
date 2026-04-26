2d26cf229f22166563d7c7af9c814aeed32dc780
Damian Nowakowski <dnowakowski200@gmail.com>
2026-04-26 23:44:52 +0200

    feat: initial work on appointments

---
+++ Reservmed/Services/AppointementService.cs
@@ -0,0 +1,24 @@
+﻿using Reservmed.Common;
+using Reservmed.DTOs;
+using Reservmed.Services.Interfaces;
+
+namespace Reservmed.Services
+{
+    public class AppointementService : IAppointmentService
+    {
+        public Task<Result<int>> BookAppointmentAsync(BookingAppointmentDto appointmentData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<List<DateTime>>> GetAvailableAppointmentsAsync(int doctorId, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdateAppointmentSlotsAsync(int doctorId, List<DateTime> dateSlotsToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
